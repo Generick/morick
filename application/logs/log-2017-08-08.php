3648b9785efbd53a12e023b100dc72ece5f9e0f1
amorick <moshinem@163.com>
2017-08-08 17:50:36 +0800

    some updates

--- application/logs/log-2017-08-08.php
+++ application/logs/log-2017-08-08.php
@@ -4,3 +4,36 @@ ERROR - 2017-08-08 09:38:41 --> [methodError]:[用户未找到！]:F:\work\progr
 ERROR - 2017-08-08 09:38:44 --> [methodError]:[用户未找到！]:F:\work\program\server\application\controllers/account.php:login:{"userType":"3","platform":"1","platformId":"18255001111","password":"admin"}
 ERROR - 2017-08-08 09:50:50 --> [methodError]:[]:F:\work\program\server\application\controllers/account.php:getMobileCode:{"mobile":"15877390255","type":"2"}
 ERROR - 2017-08-08 14:36:18 --> [methodError]:[]:F:\work\program\server\application\controllers/merchant/U_merchant.php:merchantRequest:{"commodity_id":"97","userId":"34","requestType":"1"}
+ERROR - 2017-08-08 14:57:17 --> [methodError]:[]:F:\work\program\server\application\controllers/merchant/U_merchant.php:merchantRequest:{"commodity_id":"98","userId":"34","requestType":"1"}
+ERROR - 2017-08-08 16:15:32 --> user:15balance change. the transactionType:12before:6667.50 after:6667.50
+ERROR - 2017-08-08 16:15:33 --> Update user failed: 15
+ERROR - 2017-08-08 16:15:33 --> Save user failed: 15
+ERROR - 2017-08-08 16:27:55 --> user:15balance change. the transactionType:12before:6667.50 after:6667.50
+ERROR - 2017-08-08 16:27:55 --> Update user failed: 15
+ERROR - 2017-08-08 16:27:55 --> Save user failed: 15
+ERROR - 2017-08-08 16:30:30 --> user:15balance change. the transactionType:12before:6667.50 after:6667.50
+ERROR - 2017-08-08 16:30:31 --> Update user failed: 15
+ERROR - 2017-08-08 16:30:31 --> Save user failed: 15
+ERROR - 2017-08-08 16:31:22 --> user:15balance change. the transactionType:12before:6667.50 after:6667.50
+ERROR - 2017-08-08 16:31:23 --> Update user failed: 15
+ERROR - 2017-08-08 16:31:23 --> Save user failed: 15
+ERROR - 2017-08-08 16:31:59 --> user:15balance change. the transactionType:12before:6667.50 after:6667.50
+ERROR - 2017-08-08 16:31:59 --> Update user failed: 15
+ERROR - 2017-08-08 16:31:59 --> Save user failed: 15
+ERROR - 2017-08-08 16:32:20 --> user:15balance change. the transactionType:12before:6667.50 after:6667.50
+ERROR - 2017-08-08 16:32:20 --> Update user failed: 15
+ERROR - 2017-08-08 16:32:20 --> Save user failed: 15
+ERROR - 2017-08-08 16:43:09 --> [methodError]:[会话不存在！]:F:\work\program\server\application\controllers/u_user.php:getSelfInfo:[]
+ERROR - 2017-08-08 16:43:09 --> [methodError]:[重登录失败：令牌错误!]:F:\work\program\server\application\controllers/account.php:reLogin:{"userType":"1","token":"52e2ce29a2abd0ba8dc5732232cf135b"}
+ERROR - 2017-08-08 16:44:33 --> [methodError]:[openId不能为空]:F:\work\program\server\application\controllers/order/U_order.php:wxPayTMH:{"userId":"15","commodity_id":"143","clientPrice":"1","clientTime":"1502181920","buyNum":"1","payEnv":"5","returnUrl":"http:\/\/192.168.0.163\/auction\/personCenter\/orderDetail.html","openId":""}
+ERROR - 2017-08-08 16:44:34 --> [methodError]:[openId不能为空]:F:\work\program\server\application\controllers/order/U_order.php:wxPayTMH:{"userId":"15","commodity_id":"143","clientPrice":"1","clientTime":"1502181920","buyNum":"1","payEnv":"5","returnUrl":"http:\/\/192.168.0.163\/auction\/personCenter\/orderDetail.html","openId":""}
+ERROR - 2017-08-08 16:44:35 --> [methodError]:[openId不能为空]:F:\work\program\server\application\controllers/order/U_order.php:wxPayTMH:{"userId":"15","commodity_id":"143","clientPrice":"1","clientTime":"1502181920","buyNum":"1","payEnv":"5","returnUrl":"http:\/\/192.168.0.163\/auction\/personCenter\/orderDetail.html","openId":""}
+ERROR - 2017-08-08 16:44:47 --> [methodError]:[openId不能为空]:F:\work\program\server\application\controllers/order/U_order.php:wxPayTMH:{"userId":"15","commodity_id":"143","clientPrice":"1","clientTime":"1502181920","buyNum":"1","payEnv":"5","returnUrl":"http:\/\/192.168.0.163\/auction\/personCenter\/orderDetail.html","openId":""}
+ERROR - 2017-08-08 16:44:58 --> [methodError]:[openId不能为空]:F:\work\program\server\application\controllers/order/U_order.php:wxPayTMH:{"userId":"15","commodity_id":"143","clientPrice":"1","clientTime":"1502181920","buyNum":"1","payEnv":"5","returnUrl":"http:\/\/192.168.0.163\/auction\/personCenter\/orderDetail.html","openId":""}
+ERROR - 2017-08-08 16:45:18 --> [methodError]:[openId不能为空]:F:\work\program\server\application\controllers/order/U_order.php:wxPayTMH:{"userId":"15","commodity_id":"143","clientPrice":"1","clientTime":"1502181920","buyNum":"1","payEnv":"5","returnUrl":"http:\/\/192.168.0.163\/auction\/personCenter\/orderDetail.html","openId":""}
+ERROR - 2017-08-08 16:46:01 --> [methodError]:[openId不能为空]:F:\work\program\server\application\controllers/order/U_order.php:wxPayTMH:{"userId":"15","commodity_id":"143","clientPrice":"1","clientTime":"1502181920","buyNum":"1","payEnv":"5","returnUrl":"http:\/\/192.168.0.163\/auction\/personCenter\/orderDetail.html","openId":""}
+ERROR - 2017-08-08 16:46:05 --> [methodError]:[openId不能为空]:F:\work\program\server\application\controllers/order/U_order.php:wxPayTMH:{"userId":"15","commodity_id":"143","clientPrice":"1","clientTime":"1502181920","buyNum":"1","payEnv":"5","returnUrl":"http:\/\/192.168.0.163\/auction\/personCenter\/orderDetail.html","openId":""}
+ERROR - 2017-08-08 16:46:07 --> [methodError]:[openId不能为空]:F:\work\program\server\application\controllers/order/U_order.php:wxPayTMH:{"userId":"15","commodity_id":"143","clientPrice":"1","clientTime":"1502181920","buyNum":"1","payEnv":"5","returnUrl":"http:\/\/192.168.0.163\/auction\/personCenter\/orderDetail.html","openId":""}
+ERROR - 2017-08-08 16:46:07 --> [methodError]:[openId不能为空]:F:\work\program\server\application\controllers/order/U_order.php:wxPayTMH:{"userId":"15","commodity_id":"143","clientPrice":"1","clientTime":"1502181920","buyNum":"1","payEnv":"5","returnUrl":"http:\/\/192.168.0.163\/auction\/personCenter\/orderDetail.html","openId":""}
+ERROR - 2017-08-08 16:46:07 --> [methodError]:[openId不能为空]:F:\work\program\server\application\controllers/order/U_order.php:wxPayTMH:{"userId":"15","commodity_id":"143","clientPrice":"1","clientTime":"1502181920","buyNum":"1","payEnv":"5","returnUrl":"http:\/\/192.168.0.163\/auction\/personCenter\/orderDetail.html","openId":""}
+ERROR - 2017-08-08 16:46:48 --> [methodError]:[openId不能为空]:F:\work\program\server\application\controllers/order/U_order.php:wxPayTMH:{"userId":"15","commodity_id":"145","clientPrice":"4","clientTime":"1502181120","buyNum":"1","payEnv":"5","returnUrl":"http:\/\/192.168.0.163\/auction\/personCenter\/orderDetail.html","openId":""}
---
+++ database/alter_20170808.sql
@@ -0,0 +1,2 @@
+use auction_test;
+alter table mn_commodity add column videoURL text(0) default null;
